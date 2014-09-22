0fc76f9b9fbd203be5ad8107dd640affbd3aaa81
David Huie <david@nationbuilder.com>
2014-09-21 23:16:05 -0700

    Basic interface

---
+++ gobf.go
@@ -0,0 +1,90 @@
+package gobf
+
+import (
+	"encoding/binary"
+	"hash"
+	"sync"
+)
+
+type Db interface {
+	Init(uint64) error
+	SetBit(uint64, bool) error
+	GetBit(uint64) (bool, error)
+}
+
+type BloomFilter struct {
+	hash     hash.Hash64
+	db       Db
+	size     uint64
+	hashes   uint32
+	seed     []byte
+	hashLock *sync.Mutex
+}
+
+func NewBloomFilter(hash hash.Hash64, db Db, size uint64, hashes uint32, seed uint64) (*BloomFilter, error) {
+	if err := db.Init(size); err != nil {
+		return nil, err
+	}
+
+	seedBytes := make([]byte, 8)
+	binary.LittleEndian.PutUint64(seedBytes, seed)
+
+	return &BloomFilter{
+		hash:     hash,
+		db:       db,
+		size:     size,
+		hashes:   hashes,
+		seed:     seedBytes,
+		hashLock: &sync.Mutex{},
+	}, nil
+}
+
+func (bf *BloomFilter) hashBytes(b []byte) uint64 {
+	bf.hashLock.Lock()
+	defer bf.hashLock.Unlock()
+
+	bf.hash.Write(b)
+	return bf.hash.Sum64()
+}
+
+func (bf *BloomFilter) hashPayload(p []byte, num uint32) uint64 {
+	numBytes := make([]byte, 4)
+	binary.LittleEndian.PutUint32(numBytes, num)
+
+	fullPayload := append(p, bf.seed...)
+	fullPayload = append(fullPayload, numBytes...)
+
+	return bf.hashBytes(fullPayload)
+}
+
+func (bf *BloomFilter) hashAndSetToBool(p []byte, b bool) error {
+	for i := uint32(0); i < bf.hashes; i++ {
+		h := bf.hashPayload(p, i)
+		if err := bf.db.SetBit(h, b); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (bf *BloomFilter) Insert(p []byte) error {
+	return bf.hashAndSetToBool(p, true)
+}
+
+func (bf *BloomFilter) Delete(p []byte) error {
+	return bf.hashAndSetToBool(p, false)
+}
+
+func (bf *BloomFilter) Present(p []byte) (bool, error) {
+	for i := uint32(0); i < bf.hashes; i++ {
+		h := bf.hashPayload(p, i)
+		val, err := bf.db.GetBit(h)
+		if err != nil {
+			return false, err
+		}
+		if !val {
+			return false, nil
+		}
+	}
+	return true, nil
+}
