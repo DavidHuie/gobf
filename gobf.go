329c009c9aebb65be6e3103e0e2b3f9b0e40a235
David Huie <david@nationbuilder.com>
2014-09-22 18:51:33 -0700

    Comments

--- gobf.go
+++ gobf.go
@@ -10,8 +10,11 @@ import (
 )
 
 type Db interface {
-	GetBit(uint64) (bool, error)
+	// Initializes the data store to contain the capacity specified by
+	// the input uint
 	Init(uint64) error
+
+	GetBit(uint64) (bool, error)
 	SetBit(uint64, bool) error
 }
 
@@ -24,6 +27,7 @@ type BloomFilter struct {
 	size     uint64
 }
 
+// Returns a completely customizable bloom filter
 func New(db Db, hash hash.Hash64, hashes uint32, seed uint64, size uint64) (*BloomFilter, error) {
 	if err := db.Init(size); err != nil {
 		return nil, err
@@ -46,6 +50,7 @@ const (
 	defaultSeed = 1337
 )
 
+// Returns an in-memory bloom filter that builds on the FNV hash
 func NewDefault(hashes uint32, size uint64) (*BloomFilter, error) {
 	seedBytes := make([]byte, 8)
 	binary.LittleEndian.PutUint64(seedBytes, defaultSeed)
@@ -65,6 +70,7 @@ func NewDefault(hashes uint32, size uint64) (*BloomFilter, error) {
 	}, nil
 }
 
+// Returns the a hash of the input bytes
 func (bf *BloomFilter) hashBytes(b []byte) uint64 {
 	bf.hashLock.Lock()
 	defer bf.hashLock.Unlock()
@@ -75,6 +81,7 @@ func (bf *BloomFilter) hashBytes(b []byte) uint64 {
 	return bf.hash.Sum64()
 }
 
+// Hashes the input bytes taking into account the seed and the hash number
 func (bf *BloomFilter) hashPayload(p []byte, num uint32) uint64 {
 	numBytes := make([]byte, 4)
 	binary.LittleEndian.PutUint32(numBytes, num)
@@ -85,6 +92,7 @@ func (bf *BloomFilter) hashPayload(p []byte, num uint32) uint64 {
 	return bf.hashBytes(fullPayload) % bf.size
 }
 
+// Sets a key to either true || false in the bloom filter
 func (bf *BloomFilter) setKeyToBool(key []byte, b bool) error {
 	for i := uint32(0); i < bf.hashes; i++ {
 		h := bf.hashPayload(key, i)
@@ -95,17 +103,20 @@ func (bf *BloomFilter) setKeyToBool(key []byte, b bool) error {
 	return nil
 }
 
-func (bf *BloomFilter) Insert(p []byte) error {
-	return bf.setKeyToBool(p, true)
+// Inserts a key into the bloom filter
+func (bf *BloomFilter) Insert(key []byte) error {
+	return bf.setKeyToBool(key, true)
 }
 
-func (bf *BloomFilter) Delete(p []byte) error {
-	return bf.setKeyToBool(p, false)
+// Deletes a key from the bloom filter
+func (bf *BloomFilter) Delete(key []byte) error {
+	return bf.setKeyToBool(key, false)
 }
 
-func (bf *BloomFilter) Present(p []byte) (bool, error) {
+// Returns true if the key is present in the bloom filter
+func (bf *BloomFilter) Present(key []byte) (bool, error) {
 	for i := uint32(0); i < bf.hashes; i++ {
-		h := bf.hashPayload(p, i)
+		h := bf.hashPayload(key, i)
 		val, err := bf.db.GetBit(h)
 		if err != nil {
 			return false, err
